6084094d07e42229675ff599392f3a8ecc924d30
Ravin D <ravin.d3107@outlook.com>
2024-07-30 12:54:07 +0530

    Changed reqs

--- myproject/build_files.sh
+++ myproject/build_files.sh
@@ -1,3 +1,4 @@
 # build_files.sh
+python3.12 -m pip install --upgrade pip
 pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
